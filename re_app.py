98e9ae3f93d6c3c0a9fd73210f2ec311bde7b820
= <=>
2026-01-29 11:39:18 +0300

    fix error in pattern

--- re_app.py
+++ re_app.py
@@ -1,13 +1,19 @@
 import re
 
 
-# Define the Regular experison pattern that will used with sample
-regex_email_pattern = r"[a-zA-z0-9]+@[a-zA-z]+\.(com|edu|net|gov)"
-regex_credit_pattern = r"[0-9]{4}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}"
-regex_phone_pattern = r"(\+[1-9]{3})?\d{3}-?\d{3}-?\d{4,6}"
-regex_URL_pattern = r"(https:\/\/)?(www\.)?[a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]{3}"
+# Define the Regular expression pattern that will used with sample
+regex_email_pattern = r"[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z]{2,}"
+regex_credit_pattern = r"\b[0-9]{4}[-\s]?[0-9]{4}[-\s]?[0-9]{4}[-\s]?[0-9]{4}[-\s]?\b"
+regex_phone_pattern = r"(?:\+\d{1,3}[\s.-]?)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}"
+regex_URL_pattern = r"https?://(?:www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:/[^\s]*)"
+                    
 
-
+# using try/except to open the file sample
+try:
+    with open("sample.txt", "r") as regex_sample:
+        sample = regex_sample.read()
+except FileNotFoundError:
+    print("There is error: sample.txt file not found")
 
 
 
