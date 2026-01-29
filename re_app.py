8ced9f9c7f30c4d99319d87414e4ffdad0776264
= <=>
2026-01-29 10:53:44 +0300

    basic strucure

---
+++ re_app.py
@@ -0,0 +1,13 @@
+import re
+
+
+# Define the Regular experison pattern that will used with sample
+regex_email_pattern = r"[a-zA-z0-9]+@[a-zA-z]+\.(com|edu|net|gov)"
+regex_credit_pattern = r"[0-9]{4}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}"
+regex_phone_pattern = r"(\+[1-9]{3})?\d{3}-?\d{3}-?\d{4,6}"
+regex_URL_pattern = r"(https:\/\/)?(www\.)?[a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]{3}"
+
+
+
+
+
